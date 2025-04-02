7ad023983ee82c53bbc79cc7d793a9c36d68845b
Jonathan Moya <djgrovezone@gmail.com>
2025-04-02 16:09:26 +0200

    Test transloco and primeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslocoModule } from '@jsverse/transloco';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, ButtonModule],
+    imports: [RouterOutlet, ButtonModule, TranslocoModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'sequenceTechnicalTest';
+  title = 'Songs data collector App - Sequence';
 }
