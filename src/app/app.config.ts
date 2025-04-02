440c3e40d1d445774b8032d7326f3c047f158f68
Jonathan Moya <djgrovezone@gmail.com>
2025-04-02 16:08:25 +0200

    This commit introduces new configurations for Transloco (i18n), HttpClient, and PrimeNG (UI library) in the app.config.ts file. These changes enable internationalization, HTTP client functionality, and a consistent UI theme across the application.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,39 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode  } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
+import { provideHttpClient } from '@angular/common/http';
+import { provideTransloco } from '@jsverse/transloco';
+
+import { TranslocoHttpLoader } from './transloco-loader';
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+
+import { AvailablesLanguajes, Languages } from '../../transloco.config';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes), 
+    provideClientHydration(),
+    provideHttpClient(),
+    provideTransloco({
+      config: {
+        availableLangs: AvailablesLanguajes,
+        defaultLang: Languages.ES,
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader,
+    }),
+    provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: Aura
+            }
+        })
+  ]
 };
