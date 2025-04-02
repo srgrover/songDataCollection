62cda6413cfa06c9d9ee24db0b0cacca5f5f6f2c
Jonathan Moya <djgrovezone@gmail.com>
2025-04-02 13:20:05 +0200

    Removed complex template and styles from app component, replaced with a simple button component. Updated styles to use CSS instead of SCSS and integrated TailwindCSS for global styling. Added PrimeNG button module for UI components.

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,19 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
+    provideServerRendering(),
+    provideAnimationsAsync(),
+        providePrimeNG({ 
+            theme: {
+                preset: Aura
+            }
+        })
   ]
 };
 
