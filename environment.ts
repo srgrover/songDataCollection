b8f67b419ae7a42bec3b5217343d5a7333f31092
Jonathan Moya Moreno <djgrovezone@gmail.com>
2025-04-03 16:01:47 +0200

    Revert "Api service songs"

--- environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  API_URL: process.env['API_URL'],
-};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TranslocoModule } from '@jsverse/transloco';
 import { ButtonModule } from 'primeng/button';
-import { SideMenuComponent } from "./shared/components/ui/side-menu/side-menu.component";
-import { TopMenuComponent } from "./shared/components/ui/top-menu/top-menu.component";
-import { FooterComponent } from "./shared/components/ui/footer/footer.component";
-import { PanelModule } from 'primeng/panel';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, ButtonModule, TranslocoModule, SideMenuComponent, TopMenuComponent, FooterComponent, PanelModule],
+    imports: [RouterOutlet, ButtonModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
-
 export class AppComponent {
-  title = 'Songs data collector App - Sequence';
+  title = 'sequenceTechnicalTest';
 }
