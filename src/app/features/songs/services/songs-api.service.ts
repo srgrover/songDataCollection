0483ab3cd39d23b15c912d9b114aa26c5fbe91a2
Jonathan Moya <djgrovezone@gmail.com>
2025-04-04 13:00:50 +0200

    fix(songs-api): correct environment import path

--- src/app/features/songs/services/songs-api.service.ts
+++ src/app/features/songs/services/songs-api.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Song } from '../../../core/models/song.model';
-import { environment } from '../../../../../environment';
+import { environment } from '../../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
