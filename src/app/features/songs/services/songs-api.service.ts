f6184ef2a5a0e294f5037afab1890969f756135d
Jonathan Moya <djgrovezone@gmail.com>
2025-04-07 14:07:03 +0200

    refactor(songs-api): correct environment import path and fix URL assignment

--- src/app/features/songs/services/songs-api.service.ts
+++ src/app/features/songs/services/songs-api.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Song } from '../../../core/models/song.model';
-import { environment } from '../../../../../environment';
+import { environment } from '../../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class SongsApiService {
-  URL = `${environment.API_URL}/songs` || '';
+  URL = environment.API_URL ? `${environment.API_URL}/songs` : '';
 
   constructor(private http: HttpClient) { }
 
@@ -22,13 +22,13 @@ export class SongsApiService {
     return this.http.get<Song>(url);
   }
 
-  addSong(book: Song): Observable<Song> {
-    return this.http.post<Song>(this.URL, book);
+  addSong(song: Song): Observable<Song> {
+    return this.http.post<Song>(this.URL, song);
   }
 
-  updateSong(id: number, book: Song): Observable<Song> {
+  updateSong(id: number, song: Song): Observable<Song> {
     const url = `${this.URL}/${id}`;
-    return this.http.put<Song>(url, book);
+    return this.http.put<Song>(url, song);
   }
 
   deleteSong(id: number): Observable<any> {
