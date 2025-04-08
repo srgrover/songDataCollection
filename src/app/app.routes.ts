7d14a18f88d0632f701324a06fdbae96fbc690b3
Jonathan Moya <djgrovezone@gmail.com>
2025-04-08 11:36:09 +0200

    feat(routes): add new routes for song creation and editing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,19 @@
 import { Routes } from '@angular/router';
 import { SongsComponent } from './features/songs/pages/songs/songs.component';
 import { SongComponent } from './features/songs/pages/song/song.component';
+import { SongFormComponent } from './features/songs/pages/song-form/song-form.component';
 
 export const routes: Routes = [
   { path: 'songs', component: SongsComponent },
-  { path:'songs/:id', component: SongComponent },
-  { path: '', redirectTo: '/songs', pathMatch: 'full' }, 
-  { path: '**', redirectTo: '/songs' }
+  { path: 'songs/:id', component: SongComponent },
+  {
+    path: 'songs/new',
+    component: SongFormComponent,
+  },
+  {
+    path: 'songs/edit/:id',
+    component: SongFormComponent,
+  },
+  { path: '', redirectTo: '/songs', pathMatch: 'full' },
+  { path: '**', redirectTo: '/songs' },
 ];
