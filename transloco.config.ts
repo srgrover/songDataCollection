998aac82ca794bc2b332c9731590ced8df30d40b
Jonathan Moya <djgrovezone@gmail.com>
2025-04-08 12:00:19 +0200

    refactor(transloco): update language definitions and mapping

--- transloco.config.ts
+++ transloco.config.ts
@@ -1,18 +1,25 @@
 import {TranslocoGlobalConfig} from '@jsverse/transloco-utils';
+import { LangDefinition } from '@jsverse/transloco/lib/types';
     
 export enum Languages {
   EN = 'en',
   ES = 'es',
 }
 
-export const AvailablesLanguajes = [
-  Languages.EN,
-  Languages.ES,
-]
+export const AvailablesLanguajes: LangDefinition[] = [
+  {
+    id: Languages.EN,
+    label: 'English',
+  },
+  {
+    id: Languages.ES,
+    label: 'Español',
+  },
+];
 
 const config: TranslocoGlobalConfig = {
   rootTranslationsPath: 'src/assets/i18n/',
-  langs: AvailablesLanguajes,
+  langs: AvailablesLanguajes.map((lang) => lang.id),
   defaultLang: Languages.ES,
   keysManager: {},
 };
